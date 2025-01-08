5d48bcc2e4a7dafd095a2c22cd2f96d9cf437634
rex-connor-dwight <rexconnordwight@gmail.com>
2025-01-08 13:16:32 +0100

    day seven commit

---
+++ daily-learning/January/day-07/Exercise/day07.js
@@ -0,0 +1,81 @@
+const lastName = `Dwight`;
+
+const firstName = `Rex-Connor`;
+
+const age = 25;
+
+const favoriteColor = `blue`;
+
+const date = new Date();
+
+const birthYear = date.getFullYear() - age;
+
+const iLoveChocolate = true;
+
+const schoolName =`Miva University`;
+
+const favoriteMovie = `The Matrix`;
+
+const counrtyOfOrigin = `Nigeria`;
+
+const numberOne = 3;
+const numberTwo = 5;
+const totalNumber = numberOne + numberTwo;
+
+const length = 25;
+const perimeterOfASquare = length * 4;
+
+const distance = 100;
+const time = 2;
+const speed = distance / time;
+
+const redius = 5;
+const pi = 3.14;
+const circumference = 2 * pi * redius;
+
+const number = 5;
+const square = number * number;
+
+const hours = 8;
+const hourlyPay = 400
+const totalSalary = hours * hourlyPay;
+
+const fullName = `${firstName}  ${lastName}`;
+
+const introduction = `Hi, my name is ${firstName} ${lastName}. I am ${age} years old and my favorite color is ${favoriteColor}.`;
+
+const whoIAm = `My name is ${fullName}`;
+
+const whereILive = `I live in ${counrtyOfOrigin}`;
+
+const hobby = `Coding`;
+const myHobby = `I love ${hobby}`;
+
+const whoIAmANdWhatILoveDoing = `I am ${fullName}, ${age} years old and I love ${hobby}`;
+
+const message = `I love you`;
+const greeting = `Hello, ${message}`;
+
+const productNames = `Dress`;
+const price = 100;
+const item = `${productNames} $${price}`;
+
+const quote = `make haste while the sun shine`;
+const author = `Proverb`;
+const quoteAndAuthor = `${quote} - ${author}`;
+
+const friendsName = `John`;
+const messageToFriends = `Hello ${friendsName}, how are you?`;
+
+const radomfactsOne = `I love coding`;
+const radomfactsTwo = `I hate custard`;
+const radomfactsThree = `I am a software engineer`;
+const randomFactsFour = `I speak Spanish`;
+const randomFactsFive = `i don't love people who talk too much`;
+const randomFactsSix = `I am shy`;
+const randomFactsSeven = `I don't play video games`;
+const randomFactsEight = `I love to travel`;
+const randomFactsNine = `I am a good singer`;
+const randomFactsTen = `I am a good cook`;
+
+
