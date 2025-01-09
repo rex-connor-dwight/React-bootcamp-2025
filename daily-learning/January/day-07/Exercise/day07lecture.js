3e960d0f91203c3760a3c10758225cd0c6eb0c37
rex-connor-dwight <rexconnordwight@gmail.com>
2025-01-09 14:46:59 +0100

    day eight commit

---
+++ daily-learning/January/day-07/Exercise/day07lecture.js
@@ -0,0 +1,106 @@
+
+
+
+// = is an assignment sign
+// == is and equal to sign 
+// === is a strict equal to to sign 
+// != is not equal to sign
+// !== is a strict not equal to sign
+// > is greater than sign
+// < is less than sign
+// >= is greater than or equal to sign
+// <= is less than or equal to sign
+
+
+// Universal declearation of a virable
+// ada = `girl`;
+
+// decleared with a var 
+// var edo = `State`;
+
+// decleared with a let 
+// let price = 200
+// let price = 300
+
+// decleared with const 
+// const tax = 0.07
+
+
+ 
+
+// is 
+// const tax = 0.07
+
+// const myHobby = `Coding`;
+
+// const $$$$$ = `Dollar`
+
+// const MyName = `Rex`
+
+
+// cammelCaseing
+
+// let var const class if else for while switch case break continue return try catch finally throw default
+
+
+// console.log(MyName)
+
+
+// Data Type in Js 
+
+// Primitive data type 
+
+
+
+// string
+const myName = `Rex is a boy`;
+
+
+// Boolean
+const iAmMarried = true
+
+
+// Number
+const myAge = 30;
+
+
+// undefined
+let mySchool
+
+
+// null
+  
+
+
+// Symbol
+const id = Symbol('id') 
+
+
+// Bigint
+const bigNumber = 1234567890123456789012345678901234567890n
+// const notANumber = NAN
+
+
+
+
+
+
+
+// & non premitive
+
+// arrays
+// const scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const namesOfStudent = [`Rex`, `Edo`, `Ada`, `Musa`, `Bola`, `Tola`, `Tayo`, `Tunde`, `Tunde`, `Tunde`]
+
+// const specificStudent = namesOfStudent[2];
+
+console.log( namesOfStudent[2]);
+
+
+
+
+// objects
+
+
+
