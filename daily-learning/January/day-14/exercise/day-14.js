c2632de931d147c6c350294cc16bc1ff9c401477
rex-connor-dwight <rexconnordwight@gmail.com>
2025-01-15 13:36:24 +0100

    new commit

---
+++ daily-learning/January/day-14/exercise/day-14.js
@@ -0,0 +1,21 @@
+const student = {
+    name: "Jane Doe",
+    age: 80,
+    subjects: ["Math", "Physics", "Chemistry"],
+    isGraduated: false,
+    gender: "Male",
+    man: true,
+  };
+
+
+
+//   Using Object.values() method
+//   const values = Object.values(student);
+
+//   console.log(values)
+
+// Using Object.entries() method
+// const entries = Object.entries(student);
+// console.log(entries);
+
+// This prints an arry of all the key and values in the object
