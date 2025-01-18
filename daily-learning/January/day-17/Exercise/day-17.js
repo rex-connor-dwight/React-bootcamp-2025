af00230935c2efc75c4ddf125313f3fc864c99ec
rex-connor-dwight <rexconnordwight@gmail.com>
2025-01-18 11:27:46 +0100

    day 18

---
+++ daily-learning/January/day-17/Exercise/day-17.js
@@ -0,0 +1,67 @@
+const arrowOne = () => 
+  console.log("arrowOne")
+arrowOne();
+
+const arrowTwo = () => 
+  console.log("arrowTwo")
+arrowTwo()
+
+const arrowThree = () => 
+  console.log("arrowThree")
+arrowThree()
+
+const arrowFour = () => 
+  console.log("arrowFour")
+arrowFour()
+
+const arrowFive = () => 
+  console.log("arrowFive")
+arrowFive()
+
+const arrowSix = () => 
+  console.log("arrowSix")
+arrowSix()
+
+const arrowSeven = () => 
+  console.log("arrowSeven")
+arrowSeven()
+
+const arrowEight = () => 
+  console.log("arrowEight")
+arrowEight()
+
+const arrowNine = () => 
+  console.log("arrowNine")
+arrowNine()
+
+const arrowTen = () => 
+  console.log("arrowTen")
+arrowTen()
+
+const arrowEleven = () => 
+  console.log("arrowEleven")
+arrowEleven()
+
+const arrowTwelve = () => 
+  console.log("arrowTwelve")
+arrowTwelve()
+
+const arrowThirteen = () => 
+  console.log("arrowThirteen")
+arrowThirteen()
+
+const arrowFourteen = () => 
+  console.log("arrowFourteen")
+arrowFourteen()
+
+const arrowFifteen = () => 
+  console.log("arrowFifteen")
+arrowFifteen()
+
+const arrowSixteen = () => 
+    console.log(`arrowSixteen`)
+arrowSixteen()
+
+const arrowSeventeen = () =>
+    console.log(`arrowSeventeen`)
+arrowSeventeen()
